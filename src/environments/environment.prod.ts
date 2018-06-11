1447e1ca80fba776fc8762a1701566531ca8a5c9
lmendoza48 <mendoza9.luis21@gmail.com>
2018-06-11 17:55:15 -0300

    new v4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAOiFCaYWfLpnfnn74jwxUVSMTbR1gd_YQ",
+    authDomain: "employeeregister-9efac.firebaseapp.com",
+    databaseURL: "https://employeeregister-9efac.firebaseio.com",
+    projectId: "employeeregister-9efac",
+    storageBucket: "employeeregister-9efac.appspot.com",
+    messagingSenderId: "1046348600976"
+  }
 };
