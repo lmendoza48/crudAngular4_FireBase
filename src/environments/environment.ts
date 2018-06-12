3a8c11062a776ef1b057592b22450a5f35a0ea1b
lmendoza48 <mendoza9.luis21@gmail.com>
2018-06-12 12:32:34 -0300

    commit with employee-list completed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig : {
     apiKey: "AIzaSyAOiFCaYWfLpnfnn74jwxUVSMTbR1gd_YQ",
     authDomain: "employeeregister-9efac.firebaseapp.com",
